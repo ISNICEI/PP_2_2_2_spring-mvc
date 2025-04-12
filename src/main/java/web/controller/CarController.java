ceea566a95bfb35fdc7bf05a65a238d56a97c596
ISNICEI <kzheka95@gmail.com>
2025-04-12 14:23:13 +0300

    Last try

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,23 +1,26 @@
 package web.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.service.CarServiceImpl;
+import web.model.Car;
+import web.service.CarService;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/cars")
-public class CarsController {
-    private CarServiceImpl carService;
+public class CarController {
+    private final CarService carService;
 
-    public CarsController(CarServiceImpl carService) {
+    public CarController(CarService carService) {
         this.carService = carService;
     }
 
     @GetMapping
-    public String cars(@RequestParam(value = "count",required = false) Integer count, ModelMap model) {
+    public String showCars(@RequestParam(value = "count", required = false) Integer count, Model model) {
         model.addAttribute("cars", carService.getCars(count));
         return "cars";
     }
--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,9 +1,22 @@
 package web.service;
 
+import org.springframework.stereotype.Service;
 import web.model.Car;
 
 import java.util.List;
+@Service
+public class CarService  {
+    private final List<Car> carList = List.of(
+            new Car("Cadillac", "Escalade", 2022)
+            , new Car("Mazda", "RX-8", 2007)
+            , new Car("Ford", "Mustang", 2020)
+            , new Car("Porsche", "911", 2008)
+            , new Car("Chevrolet", "Impala", 1986));
 
-public interface CarService {
-    public List<Car> getCars(Integer count);
+    public List<Car> getCars(Integer count) {
+        if (count == null || (count >= carList.size())) {
+            return carList;
+        }
+        return carList.subList(0, count);
+    }
 }
